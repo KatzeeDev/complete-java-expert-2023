10b42faa309aa49366867d4c8c8377a98d763765
Katzee <matooasr@gmail.com>
2023-07-06 18:04:27 -0400

    progress(course): complete section 7

---
+++ seccion7/PasarPorReferencia.java
@@ -0,0 +1,53 @@
+package seccion7;
+
+import java.util.Arrays;
+
+public class PasarPorReferencia {
+    public static void main(String[] args) {
+        /*
+        Inicializamos un arreglo de enteros con los valores 10, 11, 12.
+        */
+        int [] edad = {10,11,12};
+
+        System.out.println("Iniciamos el metodo main");
+        for (int i = 0; i < edad.length; i++) {
+            System.out.println("edad[i] = " + edad[i]);
+        }
+
+        System.out.println("Antes de llamar al metodo test");
+
+        /*
+        Llamamos al método test pasando el arreglo edad como argumento. En este caso, se pasa
+        una copia de la referencia al arreglo. Esto significa que el método test tiene acceso
+        al mismo arreglo en la memoria.
+        */
+        test(edad);
+
+        System.out.println("Despues de llamar al metodo test");
+
+        /*
+        Dado que el método test modificó el mismo arreglo edad, los cambios son visibles
+        después de la llamada al método test.
+        */
+        for (int i = 0; i < edad.length; i++) {
+            System.out.println("edad = " + edad[i]);
+        }
+
+        System.out.println("Finaliza el metodo main con los datos del arreglo modificados!:");
+    }
+
+    public static void test(int[] edadArr) {
+        System.out.println("Iniciamos el metodo test");
+
+        /*
+        Dentro del método test, modificamos el arreglo edadArr. Dado que edadArr es una
+        referencia al mismo arreglo edad en el método main, estas modificaciones son visibles
+        después de la llamada al método test.
+        */
+        for (int i = 0; i < edadArr.length; i++) {
+            edadArr[i] = edadArr[i] + 20;
+        }
+
+        System.out.println("Finalizamos el metodo test");
+    }
+}
---
+++ seccion7/PasarPorValor.java
@@ -0,0 +1,45 @@
+package seccion7;
+
+public class PasarPorValor {
+    public static void main(String[] args) {
+        /*
+        Declaramos y asignamos el valor 10 a una variable primitiva de tipo int llamada i.
+        */
+        int i = 10;
+        System.out.println("Iniciamos el metodo main con i  = " + i);
+
+        /*
+        Llamamos al método test pasando la variable i como argumento. Recuerda que Java pasa
+        los argumentos por valor, lo que significa que se hace una copia del valor de i (en este caso, 10)
+        y se pasa esa copia al método test.
+        */
+        test(i);
+
+        /*
+        Cuando el método test ha terminado de ejecutarse, volvemos al método main.
+        La variable i en main no ha cambiado, porque lo que se modificó en test
+        fue la copia del valor, no la variable i en sí.
+        */
+        System.out.println("Finaliza el metodo main con el valor de i = " + i);
+    }
+
+    public static void test(int i) {
+        /*
+        Ahora estamos dentro del método test. La variable i aquí es local al método test y
+        es independiente de la variable i en el método main. Inicialmente tiene el valor que
+        fue pasado como argumento al llamar a este método (una copia del valor de la variable i
+        en el método main).
+        */
+        System.out.println("Iniciamos el metodo test con i por parametro = " + i);
+
+        /*
+        Aquí cambiamos el valor de la variable i dentro del método test. Esto no afecta a
+        la variable i en el método main, porque son variables diferentes que solo comparten
+        el mismo nombre.
+        */
+        i = 35;
+        System.out.println("Finaliza el metodo test con i = " + i);
+
+        /* Esto es debido a la inmutabilidad */
+    }
+}
